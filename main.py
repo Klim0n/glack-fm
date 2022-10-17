edeb21fd903c6342c0eead069cb15a78fac13fde
KRjabinov <114918656+Klim0n@users.noreply.github.com>
2022-10-17 11:01:18 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,29 @@
+men=int(input("Введи число, а я покажу, какой месяц ему соответствует: "))
+if men>12:
+  print("Такого месяца нет")
+if men<1:
+  print("Такого месяца нет")
+if men==1:
+  print("январь")
+if men==2:
+  print("Февраль")
+if men==3:
+  print("Март")
+if men==4:
+  print("Апрель")
+if men==5:
+  print("Май")
+if men==6:
+  print("Июнь")
+if men==7:
+  print("Июль")
+if men==8:
+  print("Август")
+if men==9:
+  print("Сентябрь")
+if men==10:
+  print("Октябрь")
+if men==11:
+  print("Ноябрь")
+if men==12:
+  print("Декабрь")
